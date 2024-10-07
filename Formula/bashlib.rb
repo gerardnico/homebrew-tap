a83de8215ef36548a69220d1455dc1b781c35948
Nicolas GERARD <gerardnico@gmail.com>
2024-10-07 18:58:03 +0200

    backup

--- Formula/bashlib.rb
+++ Formula/bashlib.rb
@@ -1,8 +1,5 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Bashlib < Formula
-  desc "A collection of bash library and a loader helper"
+  desc "A collection of bash libraries"
   homepage "https://github.com/gerardnico/bash-libs"
   license "MIT"
   # https://rubydoc.brew.sh/Formula#head-class_method
@@ -13,33 +10,27 @@ class Bashlib < Formula
   # version "2.0.1"
   # sha256 "63d1de17449611f1490b1930e63cc8890f9f10c7e317f02c901e6a79236c10e2"
 
-  depends_on "bash"
+  # depends_on "bash"
   # depends_on "cmake" => :build
   
   def install
 
-    # Install libpath
-    bin.install Dir["bin/*"]
+    lib.install Dir["lib/*.sh"]
     
+    # [Ref](https://rubydoc.brew.sh/Formula#libexec-instance_method)
     # Install all .sh files from the libs directory to #{prefix}/libexec
+    # and symlink to /usr/local/lib
     # where prefix resolves to resolves to where homebrew is installed
     # 
     # Install libraries to libexec of HOMEBREW
     # ie /home/linuxbrew/.linuxbrew/libexec
-    libexec.install Dir["lib/*.sh"]
+    #libexec.install Dir["lib/*.sh"]
     
   end
 
   # https://rubydoc.brew.sh/Formula#caveats-instance_method
   def caveats
-    scripts_list = bin.children.map { |script| "  - #{script.basename}" }.join("\n")
-    <<~EOS
-      The following scripts have been installed:
-
-      #{scripts_list}
-
-    EOS
-    scripts_list = libexec.children.map { |script| "  - #{script.basename}" }.join("\n")
+    scripts_list = lib.children.map { |script| "  - #{script.basename}" }.join("\n")
     <<~EOS
       The following libraries have been installed:
 
--- Formula/shdoc.rb
+++ Formula/shdoc.rb
@@ -1,26 +1,25 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Dockenv < Formula
-  desc "Docker commands driven by environment variables"
-  homepage "https://github.com/gerardnico/dockenv"
+class Shdoc < Formula
+  desc "Documentation generator for shell scripts (bash, sh, zsh)."
+  homepage "https://github.com/reconquest/shdoc"
   license "MIT"
+  # https://raw.githubusercontent.com/reconquest/shdoc/refs/heads/master/LICENSE
   # https://rubydoc.brew.sh/Formula#head-class_method
-  head "https://github.com/gerardnico/dockenv.git", branch: "main"
-  # local head
-  # head "file:///home/admin/code/dockenv", branch: "main", using: :git
+  head "https://github.com/reconquest/shdoc.git", branch: "master"
   # Head replace the url, sha256 and version properties???
-  # url "https://github.com/bb010g/betterdiscordctl"
-  # version "2.0.1"
-  # sha256 "63d1de17449611f1490b1930e63cc8890f9f10c7e317f02c901e6a79236c10e2"
+  url "https://github.com/reconquest/shdoc/archive/refs/tags/v1.2.tar.gz"
+  version "1.2"
+  sha256 "e963e6dafdef0a214be80de3326659e0f71583b112759dd9cb0bcd2da6870c26"
 
   # depends_on "cmake" => :build
 
   def install
-    # Install all bash scripts to the bin directory
+    # Install the shdoc script
     # https://docs.brew.sh/Formula-Cookbook#bininstall-foo
     # Bin.install install the script into `/home/linuxbrew/.linuxbrew/bin/`
-    bin.install Dir["bin/*"]
+    bin.install "shdoc"
   end
 
   # https://rubydoc.brew.sh/Formula#caveats-instance_method
@@ -31,12 +30,18 @@ class Dockenv < Formula
 
       #{scripts_list}
 
-      Enjoy dockenv!
+      You need to delete this commit to have anchor working
+      https://github.com/reconquest/shdoc/pull/67
+      
+      In bash:
+      VERSION=$(brew info shdoc --json | jq --raw-output  ".[0].installed[0].version")
+      SHDOC_PATH="$(brew --cellar shdoc)/$VERSION/bin/shdoc"
+      BAD_PATTERN='gsub(/_/, "", url)'
+      sed -i "/${BAD_PATTERN//\//\\/}/d" "$SHDOC_PATH"
 
     EOS
   end
-
-  depends_on "gerardnico/tap/direnvext"
+  
   
   test do
     # `test do` will create, run in and delete a temporary directory.
